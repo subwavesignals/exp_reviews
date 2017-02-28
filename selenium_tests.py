a119bcd65fc9777d0bf5ed2d3c589115c6c72fc1
emneed <em.need@gmail.com>
2017-02-28 11:39:43 -0800

    Won't run with newest FF

--- selenium_tests.py
+++ selenium_tests.py
@@ -1,21 +1,56 @@
 import unittest
 from selenium import webdriver
+import time
 
+
+HOME_URL = "http://localhost:5000"
 
 class SeleniumTests(unittest.TestCase):
     """Tests for functionality or server and JS functions"""
 
     def setUp(self):
-        self.browser = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver.exe')
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
-    def test_index(self):
-        """Tests for index.html"""
+    def wait_for_page_load(self, timeout=30):
+        old_page = self.find_element_by_tag_name('html')
+        yield
+        WebDriverWait(self, timeout).until(staleness_of(old_page))
 
-        self.browser.get('http://localhost:5000/')
-        self.assertEqual(self.browser.title, 'EXP Reviews')git 
+    # def test_index(self):
+    #     """Tests for index.html"""
+
+    #     self.browser.get(HOME_URL)
+    #     self.assertEqual(self.browser.title, 'EXP Reviews')
+
+    def test_homelink(self):
+
+        self.browser.get(HOME_URL + '/users/1')
+        self.browser.wait_for_page_load(15)
+        homelink = self.browser.find_element_by_class_name("navbar-brand")
+
+        homelink.click()
+
+        self.browser.wait_for_page_load(15)
+        self.assertEqual(self.browser.title, 'EXP Reviews')
+
+    def test_login(self):
+
+        self.browser.get(HOME_URL + '/login')
+
+        username = self.browser.find_element_by_name("username")
+        password = self.browser.find_element_by_name("password")
+        btn = self.browser.find_element_by_name("signin")
+
+        username.send_keys("pandatoast")
+        password.send_keys("booleanbear")
+        btn.click()
+
+        self.browser.wait_for_page_load(15)
+        self.assertIn(self.browser.page_source, "Logged in")
+        self.assertEqual(self.browser.title, 'EXP Reviews')
 
 if __name__ == "__main__":
     unittest.main()
